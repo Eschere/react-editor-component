163941a197093d5fc1d53890969b77f49ad463f4
eschere <935431537@qq.com>
2019-05-12 16:47:03 +0800

    <U> 更新 代理 增加websocket转发，代理模式也能热更新

--- proxy.js
+++ proxy.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const url = require('url');
 
-http.createServer((req, res) => {
+/**
+ * 如果你要准备ueditor和ReactUEditorComponent同时开发联调
+ * 一下的配置可以解决两个项目在不同端口下运行时，iframe产生的跨域问题
+ * 将ueditor运行在8080端口，/utf8-phplu路径
+ * 将ReactUEditorComponent运行在3000端口
+ * 浏览器访问8000端口
+ */
+let server = http.createServer((req, res) => {
   const path = url.parse(req.url).path;
   console.log(path);
 
@@ -9,6 +16,18 @@ http.createServer((req, res) => {
     let request = http.request({
       port: 8080,
       path,
+      method: req.method,
+      headers: req.headers
+    }, (response) => {
+      res.writeHead(response.statusCode, response.headers);
+      response.pipe(res);
+    });
+    req.pipe(request);
+  } else if (!path.includes('websocket')) {
+    let request = http.request({
+      port: 3000,
+      path,
+      method: req.method,
       headers: req.headers
     }, (response) => {
       res.writeHead(response.statusCode, response.headers);
@@ -16,14 +35,36 @@ http.createServer((req, res) => {
     });
     req.pipe(request);
   } else {
-    let request = http.request({
-      port: 3000,
-      path,
-      headers: req.headers
-    }, (response) => {
-      res.writeHead(response.statusCode, response.headers);
-      response.pipe(res);
-    });
-    req.pipe(request);
+    res.end();
   }
 }).listen(8000);
+
+// websocket转发
+server.on('upgrade', (req, client) => {
+  const path = url.parse(req.url).path;
+  let request = http.request({
+    port: 3000,
+    path,
+    headers: req.headers
+  });
+
+  request.on('upgrade', (res, socket) => {
+    client.write(formatProxyResponse(res));
+    client.pipe(socket);
+    socket.pipe(client);
+  });
+
+  request.end();
+});
+
+function formatProxyResponse (res) {
+  const headers = res.headers;
+  const keys = Object.getOwnPropertyNames(headers);
+  let switchLine = '\r\n';
+  let response = [`HTTP/${res.httpVersion} ${res.statusCode} ${res.statusMessage}${switchLine}`];
+  keys.forEach((key) => {
+    response.push(`${key}: ${headers[key]}${switchLine}`);
+  });
+  response.push(switchLine);
+  return response.join('');
+}
